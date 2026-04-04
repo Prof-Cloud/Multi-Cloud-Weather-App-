07619734f793185fa3efd1e8694e19d094189db9
Nadir <the.fire.dragon.mac@gmail.com>
2026-04-04 11:54:19 -0400

    Add files via upload

---
+++ 4 - Cloudfront.tf
@@ -0,0 +1,127 @@
+#Cloudfront 
+
+#Creating an Origin Acess Control (OAC)
+#Used by Cloudfront to sign requests to S3 with SigV4
+resource "aws_cloudfront_origin_access_control" "OAC" {
+  name                              = "S3 OAC"
+  origin_access_control_origin_type = "s3"
+  signing_behavior                  = "always"
+  signing_protocol                  = "sigv4"
+}
+
+#Cloudfront distribution for static website
+#Attach OAC to the orgin
+
+resource "aws_cloudfront_distribution" "s3_distribution" {
+  enabled = true
+  aliases = ["www.${var.domain_name}"]
+
+  #Homepage
+  default_root_object = "index.html"
+
+  origin {
+    domain_name = aws_s3_bucket.S3_bucket.bucket_regional_domain_name
+    origin_id   = "s3_origin"
+
+    #Note must be outside s3_origin_config
+    origin_access_control_id = aws_cloudfront_origin_access_control.OAC.id
+  }
+
+  # This handles "File Not Found" or "Access Denied" by redirecting to index
+  custom_error_response {
+    error_code            = 404
+    response_code         = 200
+    response_page_path    = "/index.html"
+    error_caching_min_ttl = 10
+  }
+
+  custom_error_response {
+    error_code            = 403
+    response_code         = 200
+    response_page_path    = "/index.html"
+    error_caching_min_ttl = 10
+  }
+
+  #Cache 
+  default_cache_behavior {
+    allowed_methods  = ["GET", "HEAD"]
+    cached_methods   = ["GET", "HEAD"]
+    target_origin_id = "s3_origin"
+
+    #redirect to HTTPS
+    viewer_protocol_policy = "redirect-to-https"
+
+    #Required by cloudfront
+    forwarded_values {
+      query_string = false
+
+      cookies {
+        forward = "none"
+      }
+    }
+  }
+
+  #Price Class
+  #Controls where Cloudfront distributes your content
+  price_class = "PriceClass_100" #Cheapest - Only US, Canada and, Europe
+
+  #Restrictions
+  restrictions {
+    geo_restriction {
+      restriction_type = "none" #No geoblocking
+    }
+  }
+
+  #Viewer Certificate
+  #Required
+  viewer_certificate {
+    acm_certificate_arn      = aws_acm_certificate_validation.cert_validation.certificate_arn
+    ssl_support_method       = "sni-only"
+    minimum_protocol_version = "TLSv1.2_2021"
+  }
+
+  # Add this inside the aws_cloudfront_distribution block
+  depends_on = [aws_acm_certificate_validation.cert_validation]
+
+}
+
+resource "aws_cloudfront_distribution" "apex_redirect_distribution" {
+  origin {
+    domain_name = aws_s3_bucket_website_configuration.apex_redirect_config.website_endpoint
+    origin_id   = "ApexRedirect"
+
+    custom_origin_config {
+      http_port              = 80
+      https_port             = 443
+      origin_protocol_policy = "http-only" # S3 only speaks HTTP
+      origin_ssl_protocols   = ["TLSv1.2"]
+    }
+  }
+
+  enabled         = true
+  is_ipv6_enabled = true
+  aliases         = [var.domain_name]
+
+  default_cache_behavior {
+    allowed_methods  = ["GET", "HEAD"]
+    cached_methods   = ["GET", "HEAD"]
+    target_origin_id = "ApexRedirect"
+
+    viewer_protocol_policy = "allow-all" # This allows both HTTP and HTTPS to hit the redirect
+    forwarded_values {
+      query_string = false
+      cookies { forward = "none" }
+    }
+  }
+
+
+  restrictions {
+    geo_restriction { restriction_type = "none" }
+  }
+
+  viewer_certificate {
+   acm_certificate_arn      = aws_acm_certificate.cert.arn 
+    ssl_support_method       = "sni-only"
+    minimum_protocol_version = "TLSv1.2_2021"
+}
+}
